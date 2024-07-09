b4d7cb8a477d528b7b551c50552e7caf434f23a6
vengaiah-ai <96954654+vengaiah-ai@users.noreply.github.com>
2024-07-09 07:19:44 -0400

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -3,14 +3,14 @@
 sudo yum update -y
 sudo yum install wget -y
 sudo yum install java-1.8.0-openjdk.x86_64 -y
-sudo mkdir /app && cd /app
+sudo mkdir /opt && cd /opt
 sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/nexus-3.0.2-02-unix.tar.gz
 sudo tar -xvf nexus-3.0.2-02-unix.tar.gz
-sudo mv nexus-3* nexus
+sudo mv nexus-3.0.2-02 nexus
 sudo adduser nexus
-sudo chown -R nexus:nexus /app/nexus
-sudo chown -R nexus:nexus /app/sonatype-work
-sudo echo "run_as_user="nexus"" > /app/nexus/bin/nexus.rc
+sudo chown -R nexus:nexus /opt/nexus
+sudo chown -R nexus:nexus /opt/sonatype-work
+sudo echo "run_as_user="nexus"" > /opt/nexus/bin/nexus.rc
 sudo tee /etc/systemd/system/nexus.service > /dev/null << EOL
 [Unit]
 Description=nexus service
@@ -21,8 +21,8 @@ Type=forking
 LimitNOFILE=65536
 User=nexus
 Group=nexus
-ExecStart=/app/nexus/bin/nexus start
-ExecStop=/app/nexus/bin/nexus stop
+ExecStart=/opt/nexus/bin/nexus start
+ExecStop=/opt/nexus/bin/nexus stop
 User=nexus
 Restart=on-abort
 
